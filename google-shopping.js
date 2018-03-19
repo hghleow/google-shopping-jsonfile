06f8e1563db9f6892a9f37e9ec62c3896e2e7e8b
hghleow <gretaleow@gmail.com>
2018-03-19 18:33:08 +0800

    hw qn 1

--- google-shopping.js
+++ google-shopping.js
@@ -4,13 +4,23 @@ const jsonfile = require('jsonfile');
 const file = 'products.json'
 
 jsonfile.readFile(file, function(err, obj) {
-  console.dir(obj)
+  // console.dir(obj)
+var count = 0;
 
-  var resultFile = 'result.json'
-  var result = {name: 'JP'}
+for(var i = 0; i < obj.items.length; i++) {
+	var item = obj.items[i];
+	console.log(item.kind);
+	if (item.kind === "shopping#product") {
+ 		count++;	
+ 		console.log(count);
+ 	};
+} 
+	console.log(obj.items[0].kind);
+  // var resultFile = 'result.json'
+  // var result = {name: 'JP'}
 
-  jsonfile.writeFile(resultFile, result, function (err) {
-    console.error(err)
-  });
+  // jsonfile.writeFile(resultFile, result, function (err) {
+  //   console.error(err)
+  // });
 
 });
